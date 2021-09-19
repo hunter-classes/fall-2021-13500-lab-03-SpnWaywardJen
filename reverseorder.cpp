b8b51448860779a2b65bf1bb2b7dd59766d21a01
Jennifer <missjenab@hotmail.com>
2021-09-19 19:19:43 -0400

    Lab 3D edit

--- reverseorder.cpp
+++ reverseorder.cpp
@@ -20,23 +20,21 @@ void reverse_order(std::string date1, std::string date2) {
     std::string junk;
     getline(fin, junk);
 
-    std::string rDate;
     double eastSt, eastEl, westSt, westEl;
-    double data[365];
-    std::string date[365];
+    double rArray[365];
+    std::string rDate;
+    std::string dateArray[365];
     int x = 0;
     while(fin >> rDate >> eastSt >> eastEl >> westSt >> westEl) { 
         fin.ignore(INT_MAX, '\n');
         if(rDate >= date1 && rDate <= date2) {
-            data[x] = westEl;
-            date[x] = rDate;
+            rArray[x] = westEl;
+            dateArray[x] = rDate;
             x++;
         }
-        else if(rDate > date2)
-            break;
     }
     fin.close();
     for(int i = x-1; i >= 0; i--){
-        std::cout << date[i] << "\t" << data[i] << " ft" << std::endl;
+        std::cout << dateArray[i] << "\t" << rArray[i] << " ft" << std::endl;
     }
 }
